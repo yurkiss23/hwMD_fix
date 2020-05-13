669a1ef6fd7f511e3dfc0b3c48afd146d65b470a
yurkiss <terranet23@gmail.com>
2020-05-14 02:06:05 +0300

    add network, add android build

---
+++ Assets/Scripts/MainScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainScene : MonoBehaviour
+{
+    public void GoGame()
+    {
+        Slingshot.isFire = GameObject.Find("check_f").GetComponent<Toggle>().isOn;
+        if (!Slingshot.isFire)
+        {
+            Slingshot.firstName = GameObject.Find("txt_first").GetComponent<InputField>().text;
+            Slingshot.secondName = GameObject.Find("txt_second").GetComponent<InputField>().text;
+        }
+        else
+        {
+            Slingshot.firstName = GameObject.Find("txt_second").GetComponent<InputField>().text;
+            Slingshot.secondName = GameObject.Find("txt_first").GetComponent<InputField>().text;
+        }
+        SceneManager.LoadScene("_Scene_0");
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -1,15 +1,21 @@
-﻿using System.Collections;
+﻿using Newtonsoft.Json;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
     static private Slingshot S; //a
+    static public string firstName, secondName;
+    static public bool isFire = false;
 
     // поля, устанавливаемые в инспекторе Unity
     [Header("Set in Inspector")] // a
     public GameObject prefabProjectile;
     public float velocityMult = 8f; // a
+    public float server_fps = 0.5f;
 
     // поля, устанавливаемые динамически
     [Header("Set Dynamically")]         //a
@@ -52,21 +58,61 @@ public class Slingshot : MonoBehaviour
 
     void OnMouseDown()
     {
-        // Игрок нажал кнопку мыши, когда указатель находился над рогаткой
-        aimingMode = true;
-        // Создать снаряд
-        projectile = Instantiate(prefabProjectile) as GameObject;
-        // Поместить в точку launchPoint
-        projectile.transform.position = launchPos;
-        // Сделать его кинематическим
-        projectileRigidbody = projectile.GetComponent<Rigidbody>();
-        projectileRigidbody.isKinematic = true;
+        if (isFire)
+        {
+            // Игрок нажал кнопку мыши, когда указатель находился над рогаткой
+            aimingMode = true;
+            // Создать снаряд
+            projectile = Instantiate(prefabProjectile) as GameObject;
+            // Поместить в точку launchPoint
+            projectile.transform.position = launchPos;
+            // Сделать его кинематическим
+            projectileRigidbody = projectile.GetComponent<Rigidbody>();
+            projectileRigidbody.isKinematic = true;
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("GetRequest", server_fps);
+    }
+
+    void GetRequest()
+    {
+        // PositionCollider positionCollider = Network.GetData().Result;
+        //if (!isFire)
+        //{
+            var pc = Network.GetData(firstName);
+            if (pc != null)
+            {
+                projectile = Instantiate(prefabProjectile) as GameObject;
+
+                // Сделать его кинематическим
+                projectile.GetComponent<Rigidbody>().isKinematic = true;
+                projectileRigidbody = projectile.GetComponent<Rigidbody>();
+                projectileRigidbody.isKinematic = true;
+
+                //????????????
+                Vector3 myPos = new Vector3(pc.pos.X, pc.pos.Y, pc.pos.Z); //positionCollider.pos;//
+                projectile.transform.position = myPos;
+
+                projectileRigidbody.isKinematic = false;
+
+                //????????????
+                Vector3 v = new Vector3(pc.velocity.X, pc.velocity.Y, pc.velocity.Z);//positionCollider.velocity;
+                projectileRigidbody.velocity = v;
+
+                FollowCam.POI = projectile;
+                projectile = null;
+
+                MissionDemolition.ShotFired(); // a
+                ProjectileLine.S.poi = projectile;
+                isFire = true;
+
+            }
+        //}
+        Invoke("GetRequest", server_fps);
     }
 
     // Update is called once per frame
@@ -99,9 +145,69 @@ public class Slingshot : MonoBehaviour
             projectileRigidbody.isKinematic = false;
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
+
+            Network.PostData(secondName, projPos, projectileRigidbody.velocity);
+            isFire = false;
+
             projectile = null;
             MissionDemolition.ShotFired();
             ProjectileLine.S.poi = projectile;
         }
     }
+}
+
+
+public class Network
+{
+    public static Solider GetData(string nick)
+    {
+        string url = string.Format("http://91.238.103.45:200/api/game/{0}", nick);
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.Method = "GET";
+        var webResponse = request.GetResponse();
+        var webStream = webResponse.GetResponseStream();
+        var responseReader = new StreamReader(webStream);
+        string response = responseReader.ReadToEnd();
+        Solider pc = JsonConvert.DeserializeObject<Solider>(response);
+        responseReader.Close();
+        return pc;
+
+    }
+
+    public static void PostData(string nick, Vector3 pos, Vector3 velocity)
+    {
+        var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://91.238.103.45:200/api/game");
+        httpWebRequest.ContentType = "application/json";
+        httpWebRequest.Method = "POST";
+        using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+        {
+            Solider pc = new Solider
+            {
+                Nick = nick,
+                pos = new PosVextor3 { X = pos.x, Y = pos.y, Z = pos.z },
+                velocity = new PosVextor3 { X = velocity.x, Y = velocity.y, Z = velocity.z }
+            };
+            string json = JsonConvert.SerializeObject(pc);
+            streamWriter.Write(json);
+        }
+        var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+        using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+        {
+            var result = streamReader.ReadToEnd();
+        }
+    }
+}
+
+public class Solider
+{
+    public string Nick { get; set; }
+    public PosVextor3 pos { get; set; }
+    public PosVextor3 velocity { get; set; }
+}
+
+public class PosVextor3
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
 }
